90b01ab71c98f415976d3427cd8af206d07d3851
enkikiwi <113644925+enkikiwi@users.noreply.github.com>
2022-10-03 18:19:45 -0400

    10/3/22

---
+++ P2LAB2_Stats_SellEnki.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+prod = num1 * num2 * num3 * num4
+avg = (num1 + num2 + num3 + num4) / 4
+
+print(f'{prod:.0f}', f'{avg:.0f}')
+print(f'{prod:.3f}', f'{avg:.3f}')
